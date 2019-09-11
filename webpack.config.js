24a5aa6616891d87adc3d90b3cd45fd79f59c241
Alpha Chau <chaua0927@gmail.com>
2019-09-11 09:30:28 -0400

    Changed JS entry point to JSX, Initial version

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './wwwroot/source/app.js',
+    entry: './wwwroot/source/app.jsx',
     output: {
         path: path.resolve(__dirname, 'wwwroot/dist'),
         filename: 'bundle.js'
---
+++ wwwroot/source/app.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MoviePosterList from './components/movie-poster-list.jsx';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <MoviePosterList url = '/api/movieposters' />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#container')
+);
