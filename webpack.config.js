7466ad0ec4801d7aab47582ddf28ec72f18432cc
Alpha Chau <chaua0927@gmail.com>
2019-09-12 11:45:39 -0400

    Set up SCSS + resets, Changed src directory name

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,24 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const isDevelopment = process.env.NODE_ENV === 'development';
 
 module.exports = {
     mode: 'development',
     devtool: 'eval-source-map',
-    entry: { 'main': './wwwroot/source/app.jsx' },
+    entry: { 'main': './wwwroot/src/app.jsx' },
     output: {
         path: path.resolve(__dirname, 'wwwroot/dist'),
         filename: 'bundle.js',
         publicPath: 'dist/'
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+            chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+        }),
+
+    ],
     module: {
         rules: [
             {
@@ -19,7 +29,24 @@ module.exports = {
                         presets: ['@babel/preset-react', '@babel/preset-env']
                     }
                 }
+            },
+            {
+                test: /\.s(a|c)ss$/,
+                loader: [
+                    isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: isDevelopment
+                        }
+                    }
+                ]
             }
         ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.scss']
     }
+
 };
--- wwwroot/src/app.jsx
+++ wwwroot/src/app.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MoviePosterList from './components/movie-poster-list.jsx';
+import styles from './styles/style.scss';
 
 function App(props) {
-
     return (
             <div>
-                <MoviePosterList url = '/api/movieposters' />
+                <MoviePosterList url='/api/movieposters'/>
+                <h2 className="red">Hello world!</h2>
             </div>
     );
 }
