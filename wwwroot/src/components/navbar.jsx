31a0577944ad3c92946ba5c7a73734ef023eda29
Alpha Chau <chaua0927@gmail.com>
2019-09-13 10:57:37 -0400

    Navbar and General CSS Fixes

--- wwwroot/src/components/navbar.jsx
+++ wwwroot/src/components/navbar.jsx
@@ -3,12 +3,16 @@ import '../styles/components/navbar.scss';
 
 function NavBar(props) {
     return (
-        <nav>
+        <nav id="navbar-main">
+            <div className="nav-logo-container">
+                <a href="https://localhost:5001/home">
+                    <img alt="Movie Posters Homepage" src="../apple-touch-icon.png"/>
+                </a>
+            </div>
             <ul className="left-group">
                 <li>Left Button</li>
                 <li>Left Button #2</li>
             </ul>
-            <button>Logo</button>
             <ul className="right-group">
                 <li>Right Button</li>
                 <li>Right Button #2</li>
