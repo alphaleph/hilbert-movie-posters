620681181fb32f71ab6a9b5576c0a79a10923859
Alpha Chau <chaua0927@gmail.com>
2019-09-15 12:19:39 -0400

    Babel fixes

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,22 @@ module.exports = {
                 test: /\.tsx?$/,
                 use: {
                     loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            "@babel/preset-react",
+                            "@babel/preset-typescript",
+                            [
+                                "@babel/preset-env", {
+                                    targets: { 
+                                       "browsers": ["last 2 versions"]
+                                    },
+                                    modules: false,
+                                    useBuiltIns: "usage",
+                                    corejs: 3
+                                }
+                            ]
+                        ]
+                    }
                 }
             },
             {
