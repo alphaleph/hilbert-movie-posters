c1fb1aaf02f35a4534efbb94667a37f6e17d7a64
Alpha Chau <chaua0927@gmail.com>
2019-10-01 10:43:03 -0400

    MoviePostersList naming consistency

---
+++ wwwroot/src/components/movie-posters-list.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import { AppState, MoviePostersListProps } from '../types/index';
+import { IMoviePosterData } from '../types/api_models';
+import { UI_INIT, UI_LOADING, UI_LOADED, UI_ERROR } from '../redux/constants';
+import { selectMoviePosters } from '../redux/selectors';
+import { requestGetMoviePosters } from '../redux/actions';
+import { ListEmpty } from './list-empty';
+import { Error } from './error';
+import { Whoops } from './whoops';
+import { Loading } from './loading';
+import { MoviePoster } from './movie-poster';
+import '../styles/components/movie-posters-list';
+
+const mapStateToProps  = (state: AppState) => {
+    return {
+        moviePostersList: selectMoviePosters(state),
+        listState: state.ui.mainMoviePostersList.state
+    }
+}
+
+const mapDispatchToProps = {
+    requestGetMoviePosters
+}
+
+export const MoviePostersList: React.FunctionComponent<MoviePostersListProps> = (props: MoviePostersListProps) => {
+    if (props.listState === UI_INIT) {
+        props.requestGetMoviePosters();
+    } 
+
+    switch (props.listState) {
+        case UI_INIT || UI_LOADING:
+            return <Loading/>;
+        case UI_ERROR:
+            return <Error/>
+        case UI_LOADED:
+            console.log(props);
+            if (props.moviePostersList.length === 0) {
+                return <ListEmpty/>;
+            }
+            return (
+                <ul className="moviePostersList--main">
+                    {
+                    props.moviePostersList.map((mp: IMoviePosterData) => (
+                        <li key={mp.moviePosterId}>
+                            <MoviePoster 
+                                moviePosterName={mp.name} 
+                                moviePosterYear={mp.year} 
+                            />
+                        </li>
+                    ))
+                    }
+                </ul>
+            );
+        default:
+            return <Whoops/>
+    }
+}
+
+export const ConnectedMoviePostersList = connect(mapStateToProps, mapDispatchToProps)(MoviePostersList as unknown as any);
