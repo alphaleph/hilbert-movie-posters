c8d220a311ac32f2f290996c2a9650083ce28ab2
Alpha Chau <chaua0927@gmail.com>
2019-10-04 11:14:19 -0400

    Updated 404 page

--- wwwroot/src/components/page-not-found.tsx
+++ wwwroot/src/components/page-not-found.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import '../styles/components/page-not-found.scss';
 import Container from 'react-bootstrap/Container';
+import { UnderConstruction } from './under-construction';
 
 export function PageNotFound() {
     return (
-        <main className="my-9">
-            <Container className="page-not-found--content">
-                <h1>404 page</h1>
-                <h2>
-                    Sorry, under construction!
-                </h2>
-            </Container>
-        </main>
+        <Container className="page-not-found--content my-2">
+            <UnderConstruction pageName="404 Page"/>
+        </Container>
     );
 }
