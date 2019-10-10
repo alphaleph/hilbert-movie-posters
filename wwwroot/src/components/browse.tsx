b662bd31a1214ec4703c4f1f087f73ccb4c075b8
Alpha Chau <chaua0927@gmail.com>
2019-10-10 13:58:34 -0400

    Browse styling fixes,  Whoops update, Animations

--- wwwroot/src/components/browse.tsx
+++ wwwroot/src/components/browse.tsx
@@ -1,40 +1,41 @@
 import React from 'react';
-import '../styles/components/browse.scss';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Button from 'react-bootstrap/Button';
-import { BrowserRouter as RouteComponentProps } from 'react-router-dom';
+import { RouteComponentProps } from 'react-router-dom';
 import { MoviePostersCardColumns } from './movie-posters-card-columns';
 import { MoviesCardColumns } from './movies-card-columns';
 import { Breadcrumb } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import '../styles/components/browse.scss';
 
 export const Browse = (props: RouteComponentProps) => {
 
     let isViewingMoviePosters: boolean = props.location.pathname.endsWith(`/browse/movie-posters`);
 
     return (
-        <Container className="browse--container my-2">
-            <Row>
+        <Container className="browse__container my-2">
+            <Row className="pt-2">
                 <Col md={6}>
                     <Breadcrumb>
-                        <Breadcrumb.Item active>Browse / </Breadcrumb.Item>
+                        <Breadcrumb.Item active className="mb-0">Browse / </Breadcrumb.Item>
                     </Breadcrumb>
                 </Col>
-                <Col className="browse__browse-by-movie-button"md={6}>
+                <Col className="browse__browse-by-movie-button" md={6}>
                     <LinkContainer to={ isViewingMoviePosters ? `/browse/movies` : `/browse/movie-posters`}>
                         <Button variant="dark">Browse by { isViewingMoviePosters ? `Movie` : `Movie Poster` }</Button>
                     </LinkContainer>
                 </Col>
             </Row>
             <Row>
-                <Col className="browse--content">
-                    <h1 className="my-4">
+                <Col className="browse__content">
+                    <h1 className="mt-2 mb-5">
                         { isViewingMoviePosters ? 'Movie Posters' : 'Movies' } Collection
                     </h1>
                     { 
-                        isViewingMoviePosters ? <MoviePostersCardColumns/> : <MoviesCardColumns/>
+                        isViewingMoviePosters ? <MoviePostersCardColumns history={props.history}/> 
+                                              : <MoviesCardColumns history={props.history}/>
                     }
                 </Col>
             </Row>
--- wwwroot/src/components/whoops.tsx
+++ wwwroot/src/components/whoops.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 import '../styles/components/whoops';
 
 export const Whoops = () => {
     return (
-        <Container className="whoops--main">
-            <h1>
-                Whoops... you shouldn't be here...
-            </h1>
+        <Container className="whoops__main">
+            <Row>
+                <Col>
+                    <h1 className="mt-5">
+                        Whoops... There's a problem with the page.
+                    </h1>
+                    <p className="whoops__caption">
+                        <span className="whoops__caption-whale"><em>Whale</em></span> that's embarrassing...
+                    </p>
+                    <Image fluid src="https://res.cloudinary.com/chaua0927/image/upload/v1570710884/hilbert-poster-images/ncrbfqyxodsxivbaiely.png"/>
+                </Col>
+            </Row>
         </Container>
     );
 }
