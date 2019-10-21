bc8814ee031a5c576e143147142ab32f1a28c441
Alpha Chau <chaua0927@gmail.com>
2019-10-21 17:08:31 -0400

    AddMoviePoster template

--- wwwroot/src/components/add-movie-poster.tsx
+++ wwwroot/src/components/add-movie-poster.tsx
@@ -3,6 +3,14 @@ import '../styles/components/add-movie-poster.scss';
 import Container from 'react-bootstrap/Container';
 import { UnderConstruction } from './under-construction';
 
+// export interface IMoviePosterForm {
+//     name: string,
+//     year: number,
+//     artist: string,
+//     posterImageUrl: string,
+//     movieId?: number
+// }
+
 export function AddMoviePoster() {
     return (
         <main className="my-2">
