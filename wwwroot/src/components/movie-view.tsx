96e70e716b9757087ef360761151f10f2bb414fa
Alpha Chau <chaua0927@gmail.com>
2019-10-21 17:06:39 -0400

    Movies view and card columns templates

--- wwwroot/src/components/movie-view.tsx
+++ wwwroot/src/components/movie-view.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import { UnderConstruction } from './under-construction';
 
+// export interface IMovieData {
+//     movieId: number,
+//     name: string,
+//     year: number,
+//     moviePosters: IMoviePosterData[],
+// }
+
 export const DisconnectedMovieView = () => {
     //TODO: Check for movie posters in state based on moviePosterId in URL, if not present, then download
     return (
--- wwwroot/src/components/movies-card-columns.tsx
+++ wwwroot/src/components/movies-card-columns.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import { UnderConstruction } from './under-construction';
+import { MoviesCardColumnsProps } from '../types/index';
 
-export const DisconnectedMoviesCardColumns = () => {
+// export interface IMovieForm {
+//     name: string,
+//     year: number
+// }
+
+export const DisconnectedMoviesCardColumns = (props: MoviesCardColumnsProps) => {
     return (
         <Container>
             <UnderConstruction pageName="MoviesCardColumns"/>
